b95f80d9ceed3d88158897411f6b76418840bf68
Noah Akkad <noah.akkad@gmail.com>
2024-12-30 14:01:55 +0100

    Add a new template for the project

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ngElviHGAFnM1BHgT1zgfMlvPreIiORMVrg8imaB9PA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
